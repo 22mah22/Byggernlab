c06f799e365e36a71fe3018a433ca704fd163b30
Magne Angvik Hovdar <magneah@ntnu.no>
2020-09-09 17:44:44 +0200

    ferdig dag 3, burde moduleres og ryddes i

--- main.c
+++ main.c
@@ -14,6 +14,9 @@
 #include <stdio.h>
 #include "sram_test.c"
 
+uint8_t x_offset = 156;
+uint8_t y_offset = 164;
+
 void led_test(void){
 	
 	DDRA = 0x01;
@@ -30,73 +33,130 @@ void led_test(void){
 }
 
 const int *BASE = 0x0FFF;
-struct {
+typedef struct {
 	uint8_t OLED_CMD[512];
 	uint8_t OLED_DATA[512];
 	uint8_t ADC[1024];
 	uint8_t SRAM[2048];
 } amap;
 
+typedef struct {
+	int8_t x_val;
+	int8_t y_val;
+} joyVal;
+
+typedef struct {
+	uint8_t l_val;
+	uint8_t r_val;
+} sliderVal;
+
+void get_adc_data(amap* atmelMap, joyVal* stick, sliderVal* slider){
+	uint8_t val = 1;
+	atmelMap->ADC[1] = 0x04;
+	stick->x_val = atmelMap->ADC[1];
+	stick->y_val = atmelMap->ADC[1];
+	slider->l_val = atmelMap->ADC[1];
+	slider->r_val = atmelMap->ADC[1];
+}
+
+void calc_offset(amap* atmelMap){
+	uint8_t val = 1;
+	atmelMap->ADC[1] = val;
+	x_offset = atmelMap->ADC[1];
+	y_offset = atmelMap->ADC[1];
+}
+
+void calc_pos(joyVal* values, uint8_t x, uint8_t y){
+	if(x>x_offset){
+		values->x_val = (x-x_offset)*(100)/(255-x_offset);
+	}else if(x<x_offset){
+		values->x_val = -(x_offset-x)*(100)/x_offset;
+	}
+	if(y>y_offset){
+		values->y_val = (y-y_offset)*(100)/(255-y_offset);
+	}else if(y<y_offset){
+		values->y_val = -(y_offset-y)*(100)/y_offset;
+	}
+}
+void calc_pos_slider(sliderVal* values, uint8_t left, uint8_t right){
+	values->l_val = left*100/255;
+	values->r_val = right*100/255;
+}
+
+typedef enum{
+		LEFT,
+		UPLEFT,
+		RIGHT,
+		UPRIGHT,
+		UP,
+		DOWN,
+		NEUTRAL,
+} DIRECTION;
+
+DIRECTION calc_dir(joyVal* val){
+	//if()
+	return LEFT;
+}
+
 int main(void){
+	
+	joyVal joystick;
+	sliderVal slider;
+	volatile amap* atmelMap = (amap*) BASE;
+	
+	
 	MCUCR |= (1<<SRE);
 	
 	
 	//DDRC = 0xFF;
 	//PORTC = 0x00;
 	
-	
 	SFIOR &= ~(1<<XMM0);
 	SFIOR &= ~(1<<XMM1);
 	SFIOR |= (1<<XMM2);
-
-
-	USART_Init ( MYUBRR );
-	//SRAM_test();
 	
-	_delay_ms(500);
-	volatile char *ext_adc = (char *) 0x1000;
-	for (uint16_t i = 0; i < 0x400; i++) {
-		uint8_t some_value = rand();
-		ext_adc[i] = some_value;
-		
-	}
+	DDRA |= 0x18;
 	
-	/*DDRA = 0x01;
-
 	
-	PORTE |= (1<<PE1); // set ALE high
-	
-	_delay_ms(100);
-	
-	PORTA |= (1<<PA0);
-	
-	_delay_ms(100);
-	
-	PORTE &= ~(1<<PE1); // set ALE low (valid addr)
-	
-	_delay_ms(100);
-*/
-	
+	//calc_offset(atmelMap);
 	
 	/*
+	PORTB |= (1<<DDB1);
+	TCCR2 |= (1<<WGM20);
+	TCCR2 |= (1<<WGM20);	
+	TCCR2 |= (1<<COM21);
+	TCCR2 |= (1<<COM20);
+	*/
+
+	USART_Init ( MYUBRR );
+	SRAM_test();
+	
+	_delay_ms(500);
 	
 	while(1){
-		DDRA = 0x01;
-		PORTA |= (1<<PA0);
-		_delay_ms(1000);
-		PORTA &= ~(1<<PA0);
-		_delay_ms(1000);
-		//USART_Transmit('h');
-		//USART_Receive();
-		printf("%s", "hei");
-		led_test();
+
+		uint8_t val = 1;
+		atmelMap->ADC[1] = 0x04;
+		uint8_t valx = atmelMap->ADC[1];
+		uint8_t valy = atmelMap->ADC[1];
+		uint8_t vall = atmelMap->ADC[1];
+		uint8_t valr = atmelMap->ADC[1];
+		
+		uint8_t pin4 = PIND & (1<< PIND4);
+		uint8_t pin5 = PIND & (1<< PIND5);
+		
+		
+		//get_adc_data(atmelMap, &joystick, &slider);
+		calc_pos(&joystick,valx,valy);
+		calc_pos_slider(&slider,vall,valr);
+		printf("%d,%d,%d,%d ||| %d,%d \n",joystick.x_val,joystick.y_val,slider.l_val,slider.r_val,pin4,pin5);
 		
 	}
-		
-		
-	printf("%d",1);
-	printf("%d", USART_Receive());
-	*/
+
+		/*
+		printf("%d",1);
+		printf("%d", USART_Receive());
+		*/
 	return 0;
 }
 
