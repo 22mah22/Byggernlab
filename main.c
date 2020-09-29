ea9c7805f8ec413b93e68d1b1c88fcf5791e7472
Magne Angvik Hovdar <magneah@ntnu.no>
2020-09-29 22:35:18 +0200

    not tested yet

--- main.c
+++ main.c
@@ -17,26 +17,12 @@
 #include "protagonists.h"
 
 #include "menu.h"
+#include "menu_list.h"
 
 uint8_t currentMenu = 0;
 uint8_t menuMax = 2;
 
 const int *BASE = 0x1000;
-
-void led_test(void){
-	
-	DDRA = 0x01;
-	PORTE |= (1<<PE1); // set ALE high
-	
-	PORTA |= (1<<PA0);
-	//_delay_ms(10);
-	PORTE &= ~(1<<PE1); // set ALE low (valid addr)
-	//_delay_ms(10);
-	PORTE |= (1<<PE1); // set ALE high
-	PORTA &= ~(1<<PA0);
-	//_delay_ms(10);
-	PORTE &= ~(1<<PE1); // set ALE low (valid addr)
-}
 
 typedef struct{
 	struct menuItem* prev;
@@ -57,113 +43,32 @@ int main(void){
 	volatile amap* atmelMap = (amap*) BASE;
 	
 	MCUCR |= (1<<SRE);
-	
-	//DDRC = 0xFF;
-	//PORTC = 0x00;
-	
+
 	SFIOR &= ~(1<<XMM0);
 	SFIOR &= ~(1<<XMM1);
 	SFIOR |= (1<<XMM2);
 	
 	DDRA |= 0x18;
 	
-	//calc_offset(atmelMap);
-	
-	/*
-	PORTB |= (1<<DDB1);
-	TCCR2 |= (1<<WGM20);
-	TCCR2 |= (1<<WGM20);	
-	TCCR2 |= (1<<COM21);
-	TCCR2 |= (1<<COM20);
-	*/
 
 	USART_Init ( MYUBRR );
 	SRAM_test(); _delay_ms(5000);
 		
 	struct menuItem* head;
+	list_init();
 
-	menuItem* ptr;
-	ptr = (menuItem*)malloc(sizeof(menuItem));
-	
-	menuItem* child;
-	child = (menuItem*)malloc(sizeof(menuItem));
-	child->label = "kid";
-	child->line = 0;
-	child->next = NULL;
-	child->prev = NULL;
 
-	ptr->label = "first";
-	ptr->line = 0;
-	ptr->prev = NULL;
-	ptr->next = NULL;
-	ptr->child = child;
-	head = ptr;
-	
-	child->parent = head;
-
-	ptr = (menuItem*)malloc(sizeof(menuItem));
-
-	menuItem* temp = head;
-
-	ptr->label = "second";
-	ptr->line = 1;
-	while(temp->next != NULL){
-		temp = temp->next;
-	}
-	temp->next = ptr;
-	ptr->prev = temp;
-	ptr->next = NULL;
-
-	ptr = (menuItem*)malloc(sizeof(menuItem));
-
-	temp = head;
-
-	ptr->label = "third";
-	ptr->line = 2;
-	while(temp->next != NULL){
-		temp = temp->next;
-	}
-	temp->next = ptr;
-	ptr->prev = temp;
-	ptr->next = NULL;
 
 	oled_init(atmelMap);
 	clear_oled(atmelMap);
 	
 		
 	go_to_line(7);
-	//oled_write_string(0," Kongeriget Norge er et frit, uafhaengigt og udeleligt Rige. Dets Regjeringsform er indskraenket og arvelig-monarkisk.", 8);
-	//character_printer(atmelMap, wojak, 64, 40);
-	/*
-	go_to_line(atmelMap, 2);
-	go_to_column(atmelMap, 0);
-	for(int i = 0; i < 127; i++){
-		
-		go_to_column(atmelMap, i);
-		atmelMap->OLED_DATA[1] = 0b10101010;
-		if(i>20){
-			go_to_column(atmelMap, i-21);
-			atmelMap->OLED_DATA[1] = 0x00;
-		}
-		
-		_delay_ms(20);
-		
-	}*/
+	
 	for(int i = 0; i < 92; i++){
 		printf("\n");
-		
-		
 	}
 	
-	menu* currentMenu;
-	currentMenu = new_menu(NULL);
-	menu* submenu = new_menu(currentMenu);
-	submenu->labels[0] = "laks1";
-	submenu->labels[1] = "laks2";
-	submenu->labels[2] = "laks3";
-	submenu->labels[3] = "laks4";
-	currentMenu->links[0] = submenu;
-	write_menu_to_screen(currentMenu);
 	
 	while(1){
 		
@@ -179,41 +84,6 @@ int main(void){
 		uint8_t joy_button = PINB & (1<< PINB1);
 		
 		
-		/*if(button_check(joy_button)){
-			character_printer(atmelMap, wojak, 64, 40);
-		}*/
-		/*
-		if(joystick_direction(joystick) == RIGHT){
-			clear_oled(atmelMap);
-		}
-		if (currentMenu != 0 && joystick_direction(joystick) == UP)
-		{
-			currentMenu -= 1;
-		}
-		if (currentMenu != menuMax && joystick_direction(joystick) == DOWN)
-		{
-			currentMenu += 1;
-		}
-		
-		if(button_check(joy_button)){
-			temp = head;
-			while(temp->line != currentMenu){
-				temp = temp->next;
-			}
-			if(temp->child != NULL){
-				
-				head = temp->child;
-			}
-		}
-		ptr = head;
-		uint8_t counter = 0;
-		while(ptr != NULL){
-			printf("%s \n", ptr->label);
-			go_to_column(0);
-			oled_write_string(counter,ptr->label, 8);
-			ptr = ptr->next;
-			counter ++;
-		}*/
 			
 		
 		//get_adc_data(atmelMap, &joystick, &slider);
@@ -224,24 +94,18 @@ int main(void){
 		
 
 		_delay_ms(1);
-		DIRECTION current = joystick_direction(current, joystick);
-		if(current != NEUTRAL && current != WAITING){
-			change_selected(currentMenu, current);
-		}
+		
+		
+		direction = joystick_check(direction, joystick);
+		update_position(direction);
+		
 		if(button_check(joy_button)){
-			button_pressed(currentMenu);
+			button_action();
 		}
-		
-		
-		
 		
 		
 	}
-
-		/*
-		printf("%d",1);
-		printf("%d", USART_Receive());
-		*/
+	
 	return 0;
 }
 
